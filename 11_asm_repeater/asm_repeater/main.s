5b05a12f8d009e129fb7aeff84f3be106e2522a5
olekmali <olekmali@ieee.org>
2019-09-20 16:30:19 -0500

    Added a one file blink LED and toggle button example in gnu assembler Cleaned up all single file exampples and converted into gnu assembler Cleaned up the multifile gnu assembler example

--- 11_asm_repeater/asm_repeater/main.s
+++ 11_asm_repeater/asm_repeater/main.s
@@ -2,40 +2,26 @@
 #include <avr/io.h>
 
 .GLOBAL main
-
-.TEXT
-_start: ; expected by linker, equivalent to the "old style" .ORG 0
-    jmp main
-
-; setting up stack pointer that is needed for function calls
-;.TEXT
-;start:
-;    ldi     R16,                    0xFF && (RAMEND>>8)
-;    out     _SFR_IO_ADDR(SPH),      R16
-;    ldi     R16,                    0xFF && RAMEND
-;    out     _SFR_IO_ADDR(SPL),      R16
-;    jmp     main
-
 ; this is our main program
 .TEXT
 main:
     ; set pin 0 of port A to a push-pull output
-    ; other pins kept as inputs without pullup resistors
+    ; other pins kept as inputs without pull-up resistors
     eor     R16,                        R16
     out     _SFR_IO_ADDR(PORTA),        R16
     ldi     R16,                        0b00000001
     out     _SFR_IO_ADDR(DDRA),         R16
 
-    ; set pin 4 of port E to input with a pullup resistor
-    ; other pins kept as inputs without pullup resistors
+    ; set pin 4 of port E to input with a pull-up resistor
+    ; other pins kept as inputs without pull-up resistors
     ldi     R16,                        0b00010000
     out     _SFR_IO_ADDR(PORTE),        R16
     ldi     R16,                        0b00000000
     out     _SFR_IO_ADDR(DDRE),         R16
 
-    ; set up a constant to "negate" the pushbutton key status using ExclusiveOR
-    ; pushbutton pushed yileds input as 0
-    ; pushbutton not pusehd yileds input as 1 due to the pullup resitor 
+    ; set up a constant to "negate" the push-button key status using ExclusiveOR
+    ; push-button pushed yields input as 0
+    ; push-button not pushed yields input as 1 due to the pull-up resistor 
     ldi     R17,                        0b00010000
 
     ; main program forever loop
